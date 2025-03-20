5784264cb8a8d32b3db7c5bc43478174cccd0574
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-20 15:42:54 +0100

    a full async with try and catch and two ifs - if for !res.ok directly after the promise, if in returned data

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-.then(res => res.json())
-.then(data => { 
-    console.log(data)
-    let bkgImg = data.urls.full
-    document.body.style.backgroundImage = `url(${bkgImg})`
-})
+const getBkgImg = async () => { 
+    try{
+        const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+        if (!res.ok){
+            throw new Error(`HTTP error! Status: ${res.status}`)
+        }
+        const data = await res.json()
+
+            console.log(data)
+            let bkgImg = data.urls?.full
+            if (bkgImg){
+                document.body.style.backgroundImage = `url(${bkgImg})`
+            } else{
+                console.error("Image URL not found in data")
+            }
+    }catch (error) {
+        console.error("error fetching img", error)
+    }
+}
+
+getBkgImg()
